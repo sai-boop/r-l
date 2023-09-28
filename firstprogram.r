496d5462bc62811c59951b20cd3ed143399797b7
sai-boop <58108169+sai-boop@users.noreply.github.com>
2023-09-28 18:31:58 -0400

    Add files via upload

---
+++ firstprogram.r
@@ -0,0 +1,12 @@
+x <- 28
+y <- 98
+
+sum_result <- x + y
+diff_result <- x - y
+product_result <- x * y
+quotient_result <- x / y
+
+print(sum_result)
+print(diff_result)
+print(product_result)
+print(quotient_result)
